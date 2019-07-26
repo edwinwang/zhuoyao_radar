07aab10822fa23ef128ab9e2ee43da3ae9a87266
wangguangjun <120539330@qq.com>
2019-07-26 17:25:54 +0800

    build new

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -52,7 +52,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    publicPath: process.env.NODE_ENV === 'development' ? '/' : '/dist/',
+    publicPath: process.env.NODE_ENV === 'development' ? '/' : '/',
     filename: '[name].js',
     chunkFilename: '[name].js'
   },
